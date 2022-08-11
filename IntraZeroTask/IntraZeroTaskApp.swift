8969d0dad1bf18bbde455dec212cd75436fe1953
NourallahAhmed <NourAllahahmed7001@gmail.com>
2022-08-11 14:39:54 -0700

    Initial Commit

---
+++ IntraZeroTask/IntraZeroTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntraZeroTaskApp.swift
+//  IntraZeroTask
+//
+//  Created by NourAllah Ahmed on 11/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct IntraZeroTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
